5aa5d9ccd8af7ddecc2271b238b04f8f321543aa
kpgalligan <Kasperov22>
2014-12-28 02:16:53 -0500

    Query with sticky tasks

--- library/src/main/java/co/touchlab/android/threading/tasks/utils/TaskQueueHelper.java
+++ library/src/main/java/co/touchlab/android/threading/tasks/utils/TaskQueueHelper.java
@@ -2,6 +2,8 @@ package co.touchlab.android.threading.tasks.utils;
 
 import co.touchlab.android.threading.tasks.Task;
 import co.touchlab.android.threading.tasks.TaskQueue;
+import co.touchlab.android.threading.tasks.sticky.StickyTask;
+import co.touchlab.android.threading.tasks.sticky.StickyTaskManager;
 
 /**
  * Created by kgalligan on 10/10/14.
@@ -10,7 +12,12 @@ public class TaskQueueHelper
 {
     public static boolean hasTasksOfType(TaskQueue taskQueueActual, Class... classes)
     {
-        ClassesQuery queueQuery = new ClassesQuery(classes);
+        return hasTasksOfType(null, taskQueueActual, classes);
+    }
+
+    public static boolean hasTasksOfType(StickyTaskManager stickyTaskManager, TaskQueue taskQueueActual, Class... classes)
+    {
+        ClassesQuery queueQuery = new ClassesQuery(stickyTaskManager, classes);
         taskQueueActual.query(queueQuery);
         return queueQuery.found;
     }
@@ -19,9 +26,11 @@ public class TaskQueueHelper
     {
         boolean found = false;
         Class[] classes;
+        StickyTaskManager stickyTaskManager;
 
-        ClassesQuery(Class[] classes)
+        ClassesQuery(StickyTaskManager stickyTaskManager, Class[] classes)
         {
+            this.stickyTaskManager = stickyTaskManager;
             this.classes = classes;
         }
 
@@ -32,8 +41,20 @@ public class TaskQueueHelper
             {
                 if (task.getClass().equals(aClass))
                 {
-                    found = true;
-                    break;
+                    if(stickyTaskManager != null && task instanceof StickyTask)
+                    {
+                        StickyTask stickyTask = (StickyTask) task;
+                        if(stickyTaskManager.isTaskForMe(stickyTask))
+                        {
+                            found = true;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        found = true;
+                        break;
+                    }
                 }
             }
         }
